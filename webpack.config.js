6637554a23193127224c95a47a60bbdeeebb7916
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-06 21:16:01 +0300

    Add index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   //Указываем, где лежат исходники в нашем приложении.
@@ -16,6 +18,11 @@ module.exports = {
   },
   // Добавляем плагины
   plugins: [
-    
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      // Откуда будем брать шаблон для html, чтобы плагин его самостоятельно не генерировал
+      template: 'index.html',
+      
+    })
   ]
 };
