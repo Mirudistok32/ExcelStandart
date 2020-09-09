9f91d3503f6d23814c6833e1a663a2350dd8e5c4
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-09 15:57:42 +0300

    Create function $ which create dom element and extends dom class

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,3 +1,5 @@
+import {$} from "@core/dom";
+
 export class Excel {
   constructor(selector, options) {
     this.$el = document.querySelector(selector)
@@ -5,17 +7,13 @@ export class Excel {
   }
 
   getRoot() {
-    // afterbegin, afterend, beforebegin, beforeend
-    // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`)
-
-    // const node = document.createElement('h1')
-    // node.textContent = 'Test'
-
-    const $root = document.createElement('div')
+    const $root = $.create('div', 'excel')
 
     this.components.forEach((Component) => {
-      const component = new Component()
-      $root.insertAdjacentHTML('beforeend', component.toHTML() )
+      const $el = $.create('div', Component.className)
+      const component = new Component($el)
+      $el.innerHTML = component.toHTML()
+      $root.append($el)
     })
 
     return $root
--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,2 +1,9 @@
-export class DOMListener {}
+export class DOMListener {
+  constructor($root) {
+    if (!$root) {
+      throw new Error(`No $root provided for DOMListener`)
+    }
+    this.$root = $root
+  }
+}
 
